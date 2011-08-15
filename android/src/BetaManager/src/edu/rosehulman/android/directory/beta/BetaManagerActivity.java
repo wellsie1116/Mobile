daff33d06e0e818cdd13bf68f207cb11c31e6777
Kevin Wells <wellska1@rose-hulman.edu>
2011-08-14 23:52:43 -0700

    Register devices for beta.
    Signed-off-by: Kevin Wells <wellska1@rose-hulman.edu>

--- android/src/BetaManager/src/edu/rosehulman/android/directory/beta/BetaManagerActivity.java
+++ android/src/BetaManager/src/edu/rosehulman/android/directory/beta/BetaManagerActivity.java
@@ -2,6 +2,8 @@ package edu.rosehulman.android.directory.beta;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +23,19 @@ public class BetaManagerActivity extends Activity {
         btnUnitTests.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				btnUnitTests_onClick();
+			}
+		});
+        
+        findViewById(R.id.btnUnregister).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+			    String PREFS_FILE = "PREFS_BETA";
+			    String PREF_HAS_RUN = "HAS_RUN";
+
+				//mark that we are not registered
+				SharedPreferences prefs = getSharedPreferences(PREFS_FILE, MODE_WORLD_READABLE);
+				Editor edit = prefs.edit();
+		        edit.putBoolean(PREF_HAS_RUN, false);
+		        edit.commit();
 			}
 		});
     }
