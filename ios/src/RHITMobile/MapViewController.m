de4f1592cb22b3c7125a46926ae455116a144181
Jimmy Theis <jimmy@jetheis.com>
2011-08-27 16:17:00 -0700

    Formatting and licensing
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- ios/src/RHITMobile/MapViewController.m
+++ ios/src/RHITMobile/MapViewController.m
@@ -1,9 +1,20 @@
 //
 //  MapViewController.m
-//  RHITMobile
+//  RHIT Mobile Campus Directory
 //
-//  Created by Jimmy Theis on 8/27/11.
-//  Copyright 2011 Rose-Hulman Institute of Technology. All rights reserved.
+//  Copyright 2011 Rose-Hulman Institute of Technology
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "MapViewController.h"
@@ -18,22 +29,19 @@
 }
 */
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void) didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
     
     // Release any cached data, images, etc. that aren't in use.
 }
 
-- (void)viewDidUnload
-{
+- (void) viewDidUnload {
     [super viewDidUnload];
 
     // Release any retained subviews of the main view.
--- ios/src/RHITMobile/RHITMobileAppDelegate.h
+++ ios/src/RHITMobile/RHITMobileAppDelegate.h
@@ -1,14 +1,29 @@
 //
 //  RHITMobileAppDelegate.h
-//  RHITMobile
+//  RHIT Mobile Campus Directory
 //
-//  Created by Jimmy Theis on 8/27/11.
-//  Copyright 2011 Rose-Hulman Institute of Technology. All rights reserved.
+//  Copyright 2011 Rose-Hulman Institute of Technology
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+/// AppDelgate for the RHITMobile app. Handles application-level delegate
+/// functionality.
 
 #import <UIKit/UIKit.h>
 
-@interface RHITMobileAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate>
+@interface RHITMobileAppDelegate : NSObject
+<UIApplicationDelegate, UITabBarControllerDelegate>
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
