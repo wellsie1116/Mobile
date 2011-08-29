114c22916c73405ba871ca780a0efb675366902d
Jimmy Theis <jimmy@jetheis.com>
2011-08-28 20:59:10 -0400

    Formatting
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- ios/src/RHITMobileTests/RHPathTests.m
+++ ios/src/RHITMobileTests/RHPathTests.m
@@ -22,13 +22,22 @@
 @implementation RHPathTests
 
 - (void) testInitWithAllProperties {
-    RHNavigationNode *node1 = [[RHNavigationNode alloc] initWithLatitude:1 
-                                           longitude:2 indoors:YES floor:RHFLOOR_FIRST];
-    RHNavigationNode *node2 = [[RHNavigationNode alloc] initWithLatitude:3 
-                                           longitude:4 indoors:NO floor:RHFLOOR_SECOND];
+    RHNavigationNode *node1 = [RHNavigationNode alloc];
+    [node1 initWithLatitude:1 
+                  longitude:2 
+                    indoors:YES 
+                      floor:RHFLOOR_FIRST];
+    
+    RHNavigationNode *node2 = [RHNavigationNode alloc];
+    [node2 initWithLatitude:3
+                  longitude:4 
+                    indoors:NO 
+                      floor:RHFLOOR_SECOND];
+    
     RHPath *path = [[RHPath alloc] initWithNode1:node1
                                            node2:node2
                                         pathType:RHPATHTYPE_STANDARD];
+    
     STAssertEquals(path.node1, node1, @"RHPath.node1 not set correctly");
     STAssertEquals(path.node2, node2, @"RHPath.node2 not set correctly");
     STAssertEquals(path.pathType, RHPATHTYPE_STANDARD,
