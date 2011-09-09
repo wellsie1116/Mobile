ae4eed851d930048189c86176e7a615f3d5fec88
Bryan Watts <wattsbn@rose-hulman.edu>
2011-09-08 23:51:55 -0400

    Initial WP7 Code BingMaps = Data Binding and dynamic path request objects MapBinding = Swapping out map tile services RhitMobile = Main functionality trackme = (Research) Track user location via GPS
    Signed-off-by: Bryan Watts <wattsbn@rose-hulman.edu>

---
+++ wp7/RhitMobile/RhitMobile/Maps/BingAerial.cs
@@ -0,0 +1,10 @@
+﻿namespace MapBindingDemo.Maps
+{
+    public class BingAerial : BaseBingSource
+    {
+        public BingAerial()
+        {
+            UriFormat = "http://h{0}.ortho.tiles.virtualearth.net/tiles/h{1}.jpeg?g=203";
+        }
+    }
+}
