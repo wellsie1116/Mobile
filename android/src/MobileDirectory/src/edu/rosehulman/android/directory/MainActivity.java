cfef240580a1930bb87e1a6d8009cd4532bd9bc8
Kevin Wells <wellska1@rose-hulman.edu>
2011-09-12 21:06:18 -0400

    Show user's location on map with an overlay
    Signed-off-by: Kevin Wells <wellska1@rose-hulman.edu>

--- android/src/MobileDirectory/src/edu/rosehulman/android/directory/MainActivity.java
+++ android/src/MobileDirectory/src/edu/rosehulman/android/directory/MainActivity.java
@@ -1,6 +1,12 @@
 package edu.rosehulman.android.directory;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -8,6 +14,8 @@ import android.view.MenuItem;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.MapView.ReticleDrawMode;
 
 public class MainActivity extends MapActivity {
 	
@@ -16,7 +24,12 @@ public class MainActivity extends MapActivity {
 	private BetaManagerManager m_betaManager;
 
     private MapView m_mapView;
-	
+    
+    private LocationManager m_serviceLocation;
+    private LocationListener m_locListener;
+
+    private MyLocationOverlay m_locationOverlay;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +56,9 @@ public class MainActivity extends MapActivity {
         GeoPoint center = new GeoPoint(39483760, -87325929);
         m_mapView.getController().setCenter(center);
         m_mapView.getController().zoomToSpan(6241, 13894);
+        
+        m_locationOverlay = new MyLocationOverlay(this, m_mapView);
+        m_mapView.getOverlays().add(m_locationOverlay);
     }
     
     @Override
@@ -68,6 +84,57 @@ public class MainActivity extends MapActivity {
         default:
             return super.onOptionsItemSelected(item);
         }
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	
+        //Start the location services
+        m_serviceLocation = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        m_locListener = new LocationListener() {
+			@Override
+			public void onLocationChanged(Location location) {
+				Log.v(TAG, location.toString());
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				//TODO yell at user
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				//TODO stop yelling at user
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				if (status != LocationProvider.AVAILABLE) {
+					//TODO handle appropriately
+					//TEMPORARILY_UNAVAILABLE
+					//AVAILABLE
+				}
+			}
+		};
+        
+        m_serviceLocation.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, m_locListener);
+        m_serviceLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, m_locListener);
+        
+        // enable the location overlay
+        m_locationOverlay.enableCompass();
+        m_locationOverlay.enableMyLocation();
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	
+    	m_serviceLocation.removeUpdates(m_locListener);
+    	
+        //disable the location overlay
+        m_locationOverlay.disableCompass();
+        m_locationOverlay.disableMyLocation();
     }
 
 	@Override
