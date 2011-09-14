0434d4248ee3e1ef8bc7dd0a1a6940e69d2fc1ba
Scott Glowski <glowskst@rose-hulman.edu>
2011-09-13 20:40:09 -0400

    Server now listens for clients, parses the json request, then responds with a json response.
    Signed-off-by: Scott Glowski <glowskst@rose-hulman.edu>

---
+++ server/FakeClient/FakeClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace FakeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 4000);
+
+            var stream = client.GetStream();
+            var encoding = new ASCIIEncoding();
+            var message = new ClientRequest() { Request = 1000 };
+
+            byte[] request = encoding.GetBytes(message.Serialize());
+            stream.Write(request, 0, request.Length);
+
+            byte[] response = new byte[1000];
+            int bytesRead = stream.Read(response, 0, 1000);
+            Console.WriteLine(encoding.GetString(response, 0, bytesRead));
+
+            client.Close();
+        }
+    }
+
+    [DataContract]
+    public class ClientRequest
+    {
+        [DataMember(IsRequired = true)]
+        public int Request { get; set; }
+    }
+
+    public static class JsonUtility
+    {
+        public static T Deserialize<T>(this string json)
+            where T : class
+        {
+            MemoryStream stream = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            return (T)serializer.ReadObject(stream);
+        }
+
+        public static string Serialize<T>(this T obj)
+            where T : class
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            serializer.WriteObject(stream, obj);
+            stream.Position = 0;
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ server/RHITMobile/RHITMobile/JsonObjects.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace RHITMobile
+{
+    [DataContract]
+    public abstract class JsonObject { }
+
+    [DataContract]
+    public class ClientRequest : JsonObject
+    {
+        [DataMember(IsRequired = true)]
+        public int Request { get; set; }
+    }
+
+    [DataContract]
+    public class ErrorResponse : JsonObject
+    {
+        public ErrorResponse(string error, params object[] objects)
+        {
+            Error = String.Format(error, objects);
+        }
+
+        [DataMember(IsRequired = true)]
+        public string Error { get; set; }
+    }
+
+    [DataContract]
+    public class PointsOfInterestResponse : JsonObject
+    {
+        public PointsOfInterestResponse(params PointOfInterest[] pointsOfInterest)
+        {
+            PointsOfInterest = new List<PointOfInterest>(pointsOfInterest);
+        }
+
+        [DataMember(IsRequired = true)]
+        public List<PointOfInterest> PointsOfInterest { get; set; }
+    }
+
+    [DataContract]
+    public class PointOfInterest : JsonObject
+    {
+        public PointOfInterest(double latitude, double longitude, string name, string description)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Name = name;
+            Description = description;
+        }
+
+        [DataMember(IsRequired = true)]
+        public double Latitude { get; set; }
+        [DataMember(IsRequired = true)]
+        public double Longitude { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Name { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Description { get; set; }
+    }
+
+    public static class JsonUtility
+    {
+        public static T Deserialize<T>(this string json)
+            where T : JsonObject
+        {
+            MemoryStream stream = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            return (T)serializer.ReadObject(stream);
+        }
+
+        public static string Serialize<T>(this T obj)
+            where T : JsonObject
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            serializer.WriteObject(stream, obj);
+            stream.Position = 0;
+            using (var reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
