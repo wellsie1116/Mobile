a1ae30098d7ffb661d35e030d3484ff2d9b9ccf8
Jimmy Theis <jimmy@jetheis.com>
2011-09-13 23:11:43 -0400

    Add UILabel subclass that emulate MapKit
    refs #28 #29
    
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ ios/src/RHITMobile/RHMapLabel.m
@@ -0,0 +1,47 @@
+//
+//  RHMapLabel.m
+//  RHIT Mobile Campus Directory
+//
+//  Copyright 2011 Rose-Hulman Institute of Technology
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "RHMapLabel.h"
+
+@implementation RHMapLabel
+
+- (void)drawTextInRect:(CGRect)rect {
+    
+    CGSize shadowOffset = self.shadowOffset;
+    UIColor *textColor = self.textColor;
+    
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(c, 2);
+    CGContextSetLineJoin(c, kCGLineJoinRound);
+    
+    CGContextSetTextDrawingMode(c, kCGTextStroke);
+    CGContextTranslateCTM(c, 0, 1);
+    self.textColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8];
+    [super drawTextInRect:rect];
+    
+    CGContextSetTextDrawingMode(c, kCGTextFill);
+    CGContextTranslateCTM(c, 0, -1);
+    self.textColor = textColor;
+    self.shadowOffset = CGSizeMake(0, 0);
+    [super drawTextInRect:rect];
+    
+    self.shadowOffset = shadowOffset;
+}
+
+@end
